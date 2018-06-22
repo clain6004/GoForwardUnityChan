5f5e4fe005045722a77fdcc7525ead51ff5377ca
clain6004 <kirisame6004@gmail.com>
2018-06-22 21:55:17 +0900

    課題コミット Block.csが課題のスクリプトです。

---
+++ Assets/Block.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    private bool blockbool = false;
+
+    AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+
+        audioSource = GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+ 
+
+        if (blockbool == true)
+        {
+
+            audioSource.Play();
+
+            blockbool = false;
+
+        }
+    
+
+	}
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Ground"||collision.gameObject.tag=="Block")
+        {
+
+            blockbool=true;
+
+        }
+
+    }
+
+}
